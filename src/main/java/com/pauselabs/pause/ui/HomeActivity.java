5a095d65f7e5fb19511133075c184b169c060459
Nick <nick@pauselabs.com>
2014-12-08 06:16:27 -0500

    Got Center Tab To Work Along with others. - Privacy tab XML still overflowing into other views. Looking into that now

--- src/main/java/com/pauselabs/pause/ui/HomeActivity.java
+++ src/main/java/com/pauselabs/pause/ui/HomeActivity.java
@@ -11,8 +11,6 @@ import butterknife.Views;
  * Created by Sarafa on 12/8/14.
  */
 
-//TODO Can't get this to work properly despite it being the exact same as Settings Activity
-
 public class HomeActivity extends Activity {
 
     @Override
