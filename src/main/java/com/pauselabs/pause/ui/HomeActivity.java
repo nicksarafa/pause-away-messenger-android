d3540ea42076ac98af8bb76f930e7f8c51a3e885
Nick <nick@pauselabs.com>
2014-12-19 21:48:47 -0500

    Buttons stick to bottom on HomeView and I removed Action Bar using the Manfest

--- src/main/java/com/pauselabs/pause/ui/HomeActivity.java
+++ src/main/java/com/pauselabs/pause/ui/HomeActivity.java
@@ -2,16 +2,11 @@ package com.pauselabs.pause.ui;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.LabeledIntent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.pauselabs.R;
