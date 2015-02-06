1eaed571f40d5c35e215bc644edc1828b6f05555
Noah Passalacqua <noah.passa@gmail.com>
2015-02-05 23:18:30 -0500

    smslistener update - removed counter check (unnecessary causing Nick's phone not to work)

--- src/main/java/com/pauselabs/pause/listeners/PauseSmsListener.java
+++ src/main/java/com/pauselabs/pause/listeners/PauseSmsListener.java
@@ -20,8 +20,6 @@ public class PauseSmsListener extends ContentObserver {
 
     private final String TAG = PauseSmsListener.class.getSimpleName();
 
-    private int previousCount;
-
     private Handler handler;
 
     /**
@@ -35,61 +33,52 @@ public class PauseSmsListener extends ContentObserver {
         this.handler = handler;
 
         Injector.inject(this);
-
-        previousCount = getNewSmsCursor().getCount();
     }
 
     public void onChange(boolean selfChange){
         Cursor cursor = getNewSmsCursor();
-        int newCount = cursor.getCount();
 
-        Log.i(TAG,"prev count: " + previousCount);
-        Log.i(TAG,"new count: " + newCount);
+        if (cursor.moveToFirst()) {
+            PauseMessage newMessage;
 
-        if (newCount > previousCount) {
-            if (cursor.moveToFirst()) {
-                PauseMessage newMessage;
+            int type = cursor.getInt(cursor.getColumnIndex("type"));
+            int dateColumn = cursor.getColumnIndex("date");
+            int bodyColumn = cursor.getColumnIndex("body");
+            int addressColumn = cursor.getColumnIndex("address");
 
-                int type = cursor.getInt(cursor.getColumnIndex("type"));
-                int dateColumn = cursor.getColumnIndex("date");
-                int bodyColumn = cursor.getColumnIndex("body");
-                int addressColumn = cursor.getColumnIndex("address");
+            String from, to, number = "";
+            String message = cursor.getString(bodyColumn);
+            Long date = cursor.getLong(dateColumn);
 
-                String from, to, number = "";
-                String message = cursor.getString(bodyColumn);
-                Long date = cursor.getLong(dateColumn);
+            if (type == Telephony.Sms.MESSAGE_TYPE_SENT) {
+                Log.i(TAG,"SMS Type Sent");
+                from = "0";
+                to = cursor.getString(addressColumn);
+                number = to;
 
-                if (type == Telephony.Sms.MESSAGE_TYPE_SENT) {
-                    from = "0";
-                    to = cursor.getString(addressColumn);
-                    number = to;
+                PauseConversation activeConversation = PauseApplication.getCurrentSession().getConversationByContactNumber(number);
 
-                    PauseConversation activeConversation = PauseApplication.getCurrentSession().getConversationByContactNumber(number);
+                if (activeConversation.getMessages().size() > 0 && activeConversation.getLastMessage().getType() == Constants.Message.Type.SMS_PAUSE_OUTGOING && activeConversation.getLastMessage().getId() == cursor.getCount()) {
+                    Log.i(TAG,"Pause sent message");
 
-                    if (activeConversation.getMessages().size() > 0 && activeConversation.getLastMessage().getType() == Constants.Message.Type.SMS_PAUSE_OUTGOING && activeConversation.getLastMessage().getId() == newCount) {
-                        Log.i(TAG,"Pause sent message");
+                    PauseApplication.sendToast("Replied to message from " + activeConversation.getContactName() + " Ü");
+                } else {
+                    Log.i(TAG,"User sent message");
 
-                        PauseApplication.sendToast("Replied to message from " + activeConversation.getContactName() + " Ü");
-                    } else {
-                        Log.i(TAG,"User sent message");
-
-                        newMessage = new PauseMessage(from, to, message, date, Constants.Message.Type.SMS_OUTGOING);
-                        PauseApplication.handleMessageSent(newMessage);
-                    }
-                } else if (type == Telephony.Sms.MESSAGE_TYPE_INBOX) {
-                    Log.i(TAG, "Message received");
-
-                    PauseApplication.numSMS++;
-
-                    from = cursor.getString(addressColumn);
-                    to = "0";
-
-                    newMessage = new PauseMessage(from, to, message, date, Constants.Message.Type.SMS_INCOMING);
-                    PauseApplication.handleMessageReceived(newMessage);
+                    newMessage = new PauseMessage(from, to, message, date, Constants.Message.Type.SMS_OUTGOING);
+                    PauseApplication.handleMessageSent(newMessage);
                 }
-            }
+            } else if (type == Telephony.Sms.MESSAGE_TYPE_INBOX) {
+                Log.i(TAG, "Message received");
 
-            previousCount = newCount;
+                PauseApplication.numSMS++;
+
+                from = cursor.getString(addressColumn);
+                to = "0";
+
+                newMessage = new PauseMessage(from, to, message, date, Constants.Message.Type.SMS_INCOMING);
+                PauseApplication.handleMessageReceived(newMessage);
+            }
         }
 
         cursor.close();
