f0c79b79af415a00f0f23a74b579751e3154bc68
Nick <nick@pauselabs.com>
2015-03-24 03:33:49 -0400

    Update Bank View

--- src/main/java/com/pauselabs/pause/view/tabs/TimeBankView.java
+++ src/main/java/com/pauselabs/pause/view/tabs/TimeBankView.java
@@ -2,21 +2,14 @@ package com.pauselabs.pause.view.tabs;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 
-import com.pauselabs.R;
-
-import butterknife.InjectView;
 import butterknife.Views;
 
 /**
  * Created by Admin on 3/8/15.
  */
 public class TimeBankView extends RelativeLayout {
-
-    @InjectView(R.id.time_bank_purchase_option_list)
-    public ListView timeBankPurchaseOptionList;
 
     public TimeBankView(Context context) {
         super(context);
